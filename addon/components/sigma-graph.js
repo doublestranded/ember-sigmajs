7d6ecc388c396233034ca1070d32928cd65a5c1f
Alexander Smith <saalexantay@gmail.com>
2017-07-10 12:02:17 -0500

    attempting node component - this is going to be tricky

--- addon/components/sigma-graph.js
+++ addon/components/sigma-graph.js
@@ -1,30 +1,13 @@
 import Ember from 'ember';
+import { ParentMixin } from 'ember-composability-tools';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ParentMixin, {
   attributeBindings: ['style'],
 
   didInsertElement: function() {
     var context = this.get('element');
     var s = new sigma(context);
-    s.graph.addNode({
-      id: 'n0',
-      label: 'Hello',
-      x: 0,
-      y: 0,
-      size: 1,
-      color: '#f00'
-    }).addNode({
-      id: 'n1',
-      label: 'World !',
-      x: 1,
-      y: 1,
-      size: 1,
-      color: '#00f'
-    }).addEdge({
-      id: 'e0',
-      source: 'n0',
-      target: 'n1'
-    });
+    this.set('graph', s);
     s.refresh();
   },
 
