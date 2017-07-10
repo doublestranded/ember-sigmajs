109d94c95c2d1d3fddd37f0c47bd052eb8d6d3ee
Alexander Smith <saalexantay@gmail.com>
2017-07-10 15:34:58 -0500

    component nodes and edges; sigma-graph className

--- addon/components/sigma-graph.js
+++ addon/components/sigma-graph.js
@@ -1,30 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  attributeBindings: ['style'],
+  classNames: ['sigma-graph'],
+
+  attributeBindings: ['nodes', 'edges'],
+
+  nodes: [],
+
+  edges: [],
 
   didInsertElement: function() {
     var context = this.get('element');
     var s = new sigma(context);
-    s.graph.addNode({
-      id: 'n0',
-      label: 'Hello',
-      x: 0,
-      y: 0,
-      size: 1,
-      color: '#f00'
-    }).addNode({
-      id: 'n1',
-      label: 'World !',
-      x: 1,
-      y: 1,
-      size: 1,
-      color: '#00f'
-    }).addEdge({
-      id: 'e0',
-      source: 'n0',
-      target: 'n1'
-    });
+    for (var i in this.get('nodes')) {
+      s.graph.addNode(this.get('nodes')[i]);
+    }
+    for (var j in this.get('edges')) {
+      s.graph.addEdge(this.get('edges')[j]);
+    }
     s.refresh();
   },
 
