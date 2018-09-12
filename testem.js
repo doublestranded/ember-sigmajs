30f8e25d3dfa15f94e735aa2ba4038b3b1138c49
Alex Smith <saalexantay@gmail.com>
2018-09-12 00:47:28 -0500

    remove phantomjs

--- testem.js
+++ testem.js
@@ -1,13 +1,15 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
-  ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ],
-  "phantomjs_debug_port": 9000
+  test_page: "tests/index.html?hidepassed",
+  disable_watching: true,
+  launch_in_ci: ["Chrome"],
+  launch_in_dev: ["Chrome"],
+  browser_args: {
+    Chrome: [
+      "--headless",
+      "--disable-gpu",
+      "--remote-debugging-port=9222",
+      "--window-size=1440,900"
+    ]
+  }
 };
